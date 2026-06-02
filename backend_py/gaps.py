61d19692322ad8b35d59177d7fdb378af93b5d7b
rishikiram <Rishtyang@gmail.com>
2026-06-02 14:57:32 -0700

    Gap object created and first 3 drafted. Untested.

--- backend_py/gaps.py
+++ backend_py/gaps.py
@@ -4,16 +4,23 @@ GAP_SEVERITY_ENUM: tuple = ("unknown", "big", "not sufficient", "near sufficient
 
 @dataclass
 class Gap:
-    uid: str
+    # User Input
+    uid: str 
+    requirement_uid: str
+    # Pulled from Database
     type: str
     jurisdiction: str
-    # review_status: str
+    # Set by Class Functions
     rationale: str
     recommended_action: str
-    requirement_uid: str
-    severity: int = 1
+    # defaults
+    review_status: str = "unreviewed"
+    severity: int = 1 # big
     claim_uids: list[str] = field(default_factory=list)
-    # SEVERITY_ENUM: list = ["unknown", "big", "not sufficient", "near sufficient", "sufficient", "exceeding"]
+
+    def __post_init__(self):
+        self.jurisdiction = self.get_jurisdiction()
+        self.type = self.get_type()
 
     def severity_score(self) -> float:
         raise NotImplementedError
@@ -21,8 +28,20 @@ class Gap:
     def to_dict(self) -> dict:
         return asdict(self)
 
-    def get_claim_status(self, claim) -> str:
-         return "not implemented"
+    def get_claim_status(self, claim_uid) -> str:
+         return "TODO: implement get_claim_status"
+    def get_claim_statement(self, claim_uid) -> str:
+        return "TODO: implement get_claim_statement"
+    
+    def get_type(self, req_uid = None) -> str:
+        if req_uid is None:
+            req_uid = self.requirement_uid
+        return "TODO: need to implement get_type"
+    def get_jurisdiction(self, req_uid = None) -> str:
+        if req_uid is None:
+            req_uid = self.requirement_uid
+        return "TODO: need to implement get_jusridiction"
+
 
     def __repr__(self) -> str:
         return f"{self.__class__.__name__}(uid={self.uid}, severity={self.severity})"
@@ -33,17 +52,123 @@ class Gap_001(Gap):
     uid: str = "GAP-001"
     type: str = "Comparator uncertainty"
     jurisdiction: str = "NICE/England"
+    # severity = "big"
     rationale: str = "no randomized comparator"
     recommended_action: str = "Assess indirect comparison feasibility and RWE augmentation plan"
     requirement_uid: str = "REQ-NICE-COMP-001"
-    claim_uids: list[str] = field(default_factory=lambda: ["CLAIM-001"])
 
     SOC_comparator_claim: str = "CLAIM-006"
     direct_comparator_claim: str = "CLAIM-007"
     placebo_comparator_claim: str = "CLAIM-008"
     indirect_comparator_claim: str = "CLAIM-009"
 
-def severity_score(self) -> str:
-        if self.get_claim_status(self.SOC_comparator_claim) is "supported":
-             return GAP_SEVERITY_ENUM[-2]
-        return GAP_SEVERITY_ENUM[0]
+    def __post_init__(self):
+        # super().__post_init__() TODO use parent auto set funcs
+        self.claim_uids = [self.SOC_comparator_claim, self.direct_comparator_claim, self.placebo_comparator_claim, self.indirect_comparator_claim]
+
+    def severity_score(self) -> tuple:
+        score = 1 # GAP_SEVERITY_ENUM[4] == supported
+        new_rationale = ""
+        # If standard-of-care comparator is present
+        if not (self.get_claim_status(self.SOC_comparator_claim) == "supported"):
+            score += 2
+            new_rationale += "Standard of Care comparator is present; "
+
+        if not (self.get_claim_status(self.placebo_comparator_claim) == "supported"):
+            score += 1
+            new_rationale += "Placebo comparator is present; "
+
+        if not (self.get_claim_status(self.direct_comparator_claim) == "supported"):
+            score += 1
+            new_rationale += "Direct comparator is present; "
+
+        if not (self.get_claim_status(self.indirect_comparator_claim) == "supported"):
+            score += 1
+            new_rationale += "Indirect comparator is present; "
+
+        score = min(5, score)
+        if len(new_rationale) == 0:
+                new_rationale = "no randomized comparator"
+                
+        return (GAP_SEVERITY_ENUM[score], new_rationale)
+
+@dataclass
+class Gap_002(Gap):
+    uid: str = "GAP-002"
+    type: str = "endpoint maturity"
+    jurisdiction: str = "U.S. payer archetype"
+    # severity = "big"
+    rationale: str = "EO-006: OS immature; EO-004: ORR primary"
+    recommended_action: str = "Track longer follow-up; define durability evidence plan."
+    requirement_uid: str = "REQ-US-ENDPT-001"
+    
+    orr_claim: str = "CLAIM-001"
+    pfs_claim: str = "CLAIM-002"
+    os_claim: str = "CLAIM-010"
+
+    def __post_init__(self):
+        # super().__post_init__() TODO use parent autoset funcs
+        self.claim_uids = [self.orr_claim, self.pfs_claim, self.os_claim]
+
+    def severity_score(self) -> tuple:
+        score = 1 # GAP_SEVERITY_ENUM[4] == supported
+        new_rationale = []
+        if not (self.get_claim_status(self.os_claim) == "supported"):
+            score += 3
+            new_rationale += ["Overall Survival is shown"]
+
+        if not (self.get_claim_status(self.pfs_claim) == "supported"):
+            score += 2
+            new_rationale += ["Significant progression free survival is shown"]
+
+        if not (self.get_claim_status(self.orr_claim) == "supported"):
+            score += 1
+            new_rationale += ["A meaningful tumor response is shown"]
+
+        score = min(5, score)
+        if len(new_rationale) == 0:
+                new_rationale += ["no randomized comparator"]
+
+        return (GAP_SEVERITY_ENUM[score], "; ".join(new_rationale))
+    
+"""
+QoL/PRO evidence gap
+Medium-high
+NICE/England 
+REQ-NICE-QOL-001 
+EO-007: PRO exploratory/incomplete
+Define prospective PRO strategy and responder analysis.
+"""
+@dataclass
+class Gap_003(Gap):
+    uid: str = "GAP-003"
+    requirement_uid: str = "REQ-NICE-QOL-001"
+    
+    QoL_claim: str = "CLAIM-003"
+    PRO_claim: str = "CLAIM-011"
+    safety_and_convenience_claim: str = "CLAIM-004"
+
+    def __post_init__(self):
+        super().__post_init__()
+        self.claim_uids = [self.QoL_claim, self.PRO_claim, self.safety_and_convenience_claim]
+
+    def severity_score(self) -> tuple:
+        score = 1 # GAP_SEVERITY_ENUM[4] == supported
+        new_rationale = []
+        if self.get_claim_status(self.QoL_claim) == "supported":
+            score += 2
+            new_rationale += [self.get_claim_statement(self.QoL_claim)] # this can be done by the relational database w/ reusing the text
+
+        if  self.get_claim_status(self.PRO_claim) == "supported":
+            score += 2
+            new_rationale += [self.get_claim_statement(self.PRO_claim)]
+
+        if self.get_claim_status(self.safety_and_convenience_claim) == "supported":
+            score += 1
+            new_rationale += [self.get_claim_statement(self.safety_and_convenience_claim)]
+
+        score = min(5, score)
+        if len(new_rationale) == 0:
+                new_rationale += ["No QoL changes or PROs supporting VER-101"]
+
+        return (GAP_SEVERITY_ENUM[score], "; ".join(new_rationale))
