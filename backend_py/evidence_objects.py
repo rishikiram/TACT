c8fe20f16994c1bcaa5ce32b87c1f701055184ab
rishikiram <Rishtyang@gmail.com>
2026-06-06 17:07:21 -0700

    get comparator groups by imperfect join. non-exaustive but shouldn't produce false positives

--- backend_py/evidence_objects.py
+++ backend_py/evidence_objects.py
@@ -16,63 +16,6 @@ def get_nctids(query_uid: str) -> list[str]:
         r = [row[0] for row in cursor.fetchall()]
     return r
 
-def get_SOC_compartor_studies(query_uid: str) -> list[dict]:
-    nct_ids = get_nctids(query_uid)
-    if not nct_ids:
-        return []
-    with aact.connect_aact() as conn:
-        cur = conn.cursor()
-        # I dont want to use dssign groups, as they are not necesarilty related to the outocme grpups
-        cur.execute(
-            """
-            SELECT
-                rg.nct_id,
-                rg.group_type,
-                rg.title        AS arm_title,
-                rg.description  AS arm_description,
-                o.title         AS outcome_title,
-                o.outcome_type,
-                om.param_type,
-                om.param_value,
-                om.dispersion_type,
-                om.dispersion_value,
-                om.units
-            FROM result_groups rg
-            JOIN outcome_measurements om
-                ON  om.nct_id          = rg.nct_id
-                AND om.ctgov_group_code = rg.ctgov_group_code
-            JOIN outcomes o
-                ON  o.id = om.outcome_id
-            WHERE rg.nct_id = ANY(%s)
-              AND rg.result_type = 'Outcome'
-              AND rg.group_type IN (
-                    'Active Comparator',
-                    'Placebo Comparator',
-                    'Sham Comparator',
-                    'No Intervention'
-              )
-            ORDER BY rg.nct_id, rg.title, o.title
-            """,
-            (nct_ids,)
-        )
-        rows = cur.fetchall()
-    return [
-        {
-            "nct_id":            row[0],
-            "group_type":        row[1],
-            "arm_title":         row[2],
-            "arm_description":   row[3],
-            "outcome_title":     row[4],
-            "outcome_type":      row[5],
-            "param_type":        row[6],
-            "param_value":       row[7],
-            "dispersion_type":   row[8],
-            "dispersion_value":  row[9],
-            "units":             row[10],
-        }
-        for row in rows
-    ]
-
 def get_comparator_outcome_measurments(study_nctids: list[str]) -> list[dict]:
     with aact.connect_aact() as conn:
         cur = conn.cursor()
@@ -111,19 +54,34 @@ def get_comparator_outcome_measurments(study_nctids: list[str]) -> list[dict]:
         for row in rows
     ]
 
-def get_potential_comparator_groups(study_nctids: list[str]) -> list[dict]:
+def get_potential_X_comparator_groups(study_nctids: list[str], ) -> list[dict]:
     with aact.connect_aact() as conn:
         cur = conn.cursor()
         cur.execute(
             """
-            SELECT t.nct_id, t.group_type, t.title, t.description FROM design_groups as t 
-            WHERE t.nct_id = ANY(%s)
+            SELECT
+                rg.nct_id,
+                MIN(rg.id)           AS result_group_id,
+                MIN(dg.id)           AS design_group_id,
+                dg.group_type,
+                rg.title,
+                MIN(rg.description)
+            FROM result_groups AS rg
+            JOIN design_groups AS dg 
+                ON  dg.nct_id = rg.nct_id AND dg.title = rg.title
+            WHERE rg.nct_id = ANY(%s)
+            GROUP BY
+                rg.nct_id,
+                dg.group_type,
+                rg.title
             """,
             (study_nctids,)
         ) 
         rows = cur.fetchall()
-    r = [{"nct_id": row[0], "group_type": row[1], "title": row[2], "description": row[3]} for row in rows]
+    r = [{"nct_id": row[0], "result_group_id": row[1], "design_group_id": row[2], "group_type": row[3], "title": row[4], "description": row[5]} for row in rows]
     return r
+
+GROUP_TYPES = ("ACTIVE_COMPARATOR", "EXPERIMENTAL", "NO_INTERVENTION", "OTHER", "PLACEBO_COMPARATOR", "SHAM_COMPARATOR")
 
 def get_potential_comparator_endpoints(study_nctids: list[str]) -> list[dict]:
     with aact.connect_aact() as conn:
@@ -139,48 +97,50 @@ def get_potential_comparator_endpoints(study_nctids: list[str]) -> list[dict]:
     r = [{"nct_id": row[0], "outcome_type": row[1], "measure": row[2], "timeframe": row[3], "population": row[4], "description": row[5]} for row in rows]
     return r
 
-def get_something(study_nctids: list[str]) -> list[dict]:
-    with aact.connect_aact() as conn:
-        cur = conn.cursor()
-        cur.execute(
-            """
-            SELECT t.nct_id, t.outcome_type, t.measure, t.timeframe, t.population, t.description FROM design_outcomes AS t 
-            WHERE t.nct_id = ANY(%s)
-            """,
-            (study_nctids,)
-        )
-        rows = cur.fetchall()
-    r = [{"nct_id": row[0], "outcome_type": row[1], "measure": row[2], "timeframe": row[3], "population": row[4], "description": row[5]} for row in rows]
-    return r
 
-def get_stuff(study_nctids: list[str]) -> list[dict]:
-    with aact.connect_aact() as conn:
-        cur = conn.cursor()
-        cur.execute(
-            """
-            SELECT t.nct_id, t.outcome_type, t.measure, t.timeframe, t.population, t.description FROM design_outcomes AS t 
-            WHERE t.nct_id = ANY(%s)
-            """,
-            (study_nctids,)
-        )
-        rows = cur.fetchall()
-    r = [{"nct_id": row[0], "outcome_type": row[1], "measure": row[2], "timeframe": row[3], "population": row[4], "description": row[5]} for row in rows]
-    return r
+"""
+SELECT DISTINCT
+    rg.nct_id,
+    dg.group_type	AS design_group_type,
+    rg.title        AS rg_title,
+    rg.description  AS rg_description
+FROM result_groups AS rg
+JOIN design_groups AS dg 
+    ON  dg.nct_id = rg.nct_id AND dg.title = rg.title
+WHERE rg.nct_id = ANY(ARRAY['NCT00005037', 'NCT00005093', 'NCT00021008', 'NCT00022022', 'NCT00022243', 'NCT00029003', 'NCT00030420', 'NCT00030641', 'NCT00033722', 'NCT00039091', 'NCT00040885', 'NCT00045006', 'NCT00047840', 'NCT00054184', 'NCT00062101', 'NCT00064012', 'NCT00066768', 'NCT00073008', 'NCT00074204', 'NCT00075751', 'NCT00077415', 'NCT00114192', 'NCT00139711', 'NCT00160043', 'NCT00160069', 'NCT00161187', 'NCT00228358', 'NCT00238849', 'NCT00268489', 'NCT00278187', 'NCT00300586', 'NCT00305786', 'NCT00327288', 'NCT00343720', 'NCT00346645', 'NCT00359450', 'NCT00362882', 'NCT00388206', 'NCT00398138', 'NCT00400829', 'NCT00445952', 'NCT00447057', 'NCT00454194', 'NCT00499733', 'NCT00519831', 'NCT00520845', 'NCT00528281', 'NCT00530621', 'NCT00532155', 'NCT00534209', 'NCT00538850', 'NCT00638937', 'NCT00693992', 'NCT00697060', 'NCT00698815', 'NCT00738335', 'NCT00738881', 'NCT00754923', 'NCT00777309', 'NCT00989690', 'NCT01022671', 'NCT01057212', 'NCT01068587', 'NCT01107444', 'NCT01124669', 'NCT01168973', 'NCT01183858', 'NCT01204697', 'NCT01210053', 'NCT01248247', 'NCT01344824', 'NCT01362296', 'NCT01395758', 'NCT01438307', 'NCT01526928', 'NCT01620190', 'NCT01630733', 'NCT01658813', 'NCT01664533', 'NCT01708993', 'NCT01717105', 'NCT01725165', 'NCT01750281', 'NCT01773109', 'NCT01783197', 'NCT01783834', 'NCT01784640', 'NCT01846416', 'NCT01866410', 'NCT01915524', 'NCT01933932', 'NCT01982955', 'NCT01999673', 'NCT02041468', 'NCT02069418', 'NCT02132884', 'NCT02134990', 'NCT02152631', 'NCT02208843', 'NCT02250326', 'NCT02298153', 'NCT02330367', 'NCT02346370', 'NCT02424617', 'NCT02451865', 'NCT02463994', 'NCT02503358', 'NCT02520778', 'NCT02546986', 'NCT02592577', 'NCT02613507', 'NCT02623595', 'NCT02658097', 'NCT02673814', 'NCT02750215', 'NCT02823990', 'NCT02879760', 'NCT02950038', 'NCT02959437', 'NCT02967133', 'NCT02976740', 'NCT02981108', 'NCT03023423', 'NCT03048500', 'NCT03053297', 'NCT03085069', 'NCT03087448', 'NCT03133546', 'NCT03138889', 'NCT03158883', 'NCT03168464', 'NCT03176173', 'NCT03273790', 'NCT03285763', 'NCT03288870', 'NCT03304093', 'NCT03370159', 'NCT03382912', 'NCT03452592', 'NCT03468985', 'NCT03469960', 'NCT03502850', 'NCT03512847', 'NCT03669523', 'NCT03735121', 'NCT03801304', 'NCT03854227', 'NCT03922997', 'NCT03976375', 'NCT04023617', 'NCT04036682', 'NCT04044170', 'NCT04069936', 'NCT04154956', 'NCT04207775', 'NCT04303780', 'NCT04331626', 'NCT04364620', 'NCT04394624', 'NCT04413227', 'NCT04471415', 'NCT04471428', 'NCT04612673', 'NCT04614103', 'NCT04619004', 'NCT04640935', 'NCT04644237', 'NCT04656652', 'NCT04667234', 'NCT04685135', 'NCT04790682', 'NCT04811001', 'NCT04816214', 'NCT04868877', 'NCT04878107', 'NCT04884282', 'NCT05195632', 'NCT05255302', 'NCT05361174', 'NCT05364073', 'NCT05378763', 'NCT05403385', 'NCT05407155', 'NCT05488314', 'NCT05555732', 'NCT05577715', 'NCT05599789', 'NCT05631249', 'NCT05661240', 'NCT05796726', 'NCT05834348', 'NCT05862194', 'NCT05935774', 'NCT06048705', 'NCT06068153', 'NCT06106802', 'NCT06279728', 'NCT06388031', 'NCT06394674', 'NCT06487156', 'NCT06497556', 'NCT06523673', 'NCT06542731', 'NCT06555263', 'NCT06557967', 'NCT06558799', 'NCT06667076', 'NCT06668103', 'NCT06686771', 'NCT06690671', 'NCT06759857', 'NCT06761976', 'NCT06809764', 'NCT06813664', 'NCT06929936', 'NCT06933329', 'NCT07111104', 'NCT07130032', 'NCT07140016', 'NCT07144280', 'NCT07154368', 'NCT07164170', 'NCT07174388', 'NCT07185997', 'NCT07193160', 'NCT07213076', 'NCT07242274', 'NCT07246863', 'NCT07288034', 'NCT07363811', 'NCT07420439', 'NCT07463677', 'NCT07609251']) 
+"""
 
-def get_foo(study_nctids: list[str]) -> list[dict]:
-    with aact.connect_aact() as conn:
-        cur = conn.cursor()
-        cur.execute(
-            """
-            SELECT t.nct_id, t.outcome_type, t.measure, t.timeframe, t.population, t.description FROM design_outcomes AS t 
-            WHERE t.nct_id = ANY(%s)
-            """,
-            (study_nctids,)
-        )
-        rows = cur.fetchall()
-    r = [{"nct_id": row[0], "outcome_type": row[1], "measure": row[2], "timeframe": row[3], "population": row[4], "description": row[5]} for row in rows]
-    return r
 
+"""
+            SELECT
+                rg.nct_id,
+                -- rg.group_type,
+				dg.group_type	AS design_group_type,
+                rg.title        AS rg_title,
+                o.outcome_type,
+                o.title         AS outcome_title,
+                rg.description  AS rg_description
+                -- om.param_type,
+                -- om.param_value,
+                -- om.dispersion_type,
+                -- om.dispersion_value,
+                -- om.units
+            FROM result_groups AS rg
+			JOIN outcomes AS o
+                ON  o.id = rg.outcome_id
+            JOIN design_groups AS dg 
+                ON  dg.nct_id = rg.nct_id AND dg.title = rg.title
+			-- JOIN outcome_measurments om
+				-- ON om.outcome_id = o.id
+            WHERE rg.nct_id = ANY(ARRAY['NCT00005037', 'NCT00005093', 'NCT00021008', 'NCT00022022', 'NCT00022243', 'NCT00029003', 'NCT00030420', 'NCT00030641', 'NCT00033722', 'NCT00039091', 'NCT00040885', 'NCT00045006', 'NCT00047840', 'NCT00054184', 'NCT00062101', 'NCT00064012', 'NCT00066768', 'NCT00073008', 'NCT00074204', 'NCT00075751', 'NCT00077415', 'NCT00114192', 'NCT00139711', 'NCT00160043', 'NCT00160069', 'NCT00161187', 'NCT00228358', 'NCT00238849', 'NCT00268489', 'NCT00278187', 'NCT00300586', 'NCT00305786', 'NCT00327288', 'NCT00343720', 'NCT00346645', 'NCT00359450', 'NCT00362882', 'NCT00388206', 'NCT00398138', 'NCT00400829', 'NCT00445952', 'NCT00447057', 'NCT00454194', 'NCT00499733', 'NCT00519831', 'NCT00520845', 'NCT00528281', 'NCT00530621', 'NCT00532155', 'NCT00534209', 'NCT00538850', 'NCT00638937', 'NCT00693992', 'NCT00697060', 'NCT00698815', 'NCT00738335', 'NCT00738881', 'NCT00754923', 'NCT00777309', 'NCT00989690', 'NCT01022671', 'NCT01057212', 'NCT01068587', 'NCT01107444', 'NCT01124669', 'NCT01168973', 'NCT01183858', 'NCT01204697', 'NCT01210053', 'NCT01248247', 'NCT01344824', 'NCT01362296', 'NCT01395758', 'NCT01438307', 'NCT01526928', 'NCT01620190', 'NCT01630733', 'NCT01658813', 'NCT01664533', 'NCT01708993', 'NCT01717105', 'NCT01725165', 'NCT01750281', 'NCT01773109', 'NCT01783197', 'NCT01783834', 'NCT01784640', 'NCT01846416', 'NCT01866410', 'NCT01915524', 'NCT01933932', 'NCT01982955', 'NCT01999673', 'NCT02041468', 'NCT02069418', 'NCT02132884', 'NCT02134990', 'NCT02152631', 'NCT02208843', 'NCT02250326', 'NCT02298153', 'NCT02330367', 'NCT02346370', 'NCT02424617', 'NCT02451865', 'NCT02463994', 'NCT02503358', 'NCT02520778', 'NCT02546986', 'NCT02592577', 'NCT02613507', 'NCT02623595', 'NCT02658097', 'NCT02673814', 'NCT02750215', 'NCT02823990', 'NCT02879760', 'NCT02950038', 'NCT02959437', 'NCT02967133', 'NCT02976740', 'NCT02981108', 'NCT03023423', 'NCT03048500', 'NCT03053297', 'NCT03085069', 'NCT03087448', 'NCT03133546', 'NCT03138889', 'NCT03158883', 'NCT03168464', 'NCT03176173', 'NCT03273790', 'NCT03285763', 'NCT03288870', 'NCT03304093', 'NCT03370159', 'NCT03382912', 'NCT03452592', 'NCT03468985', 'NCT03469960', 'NCT03502850', 'NCT03512847', 'NCT03669523', 'NCT03735121', 'NCT03801304', 'NCT03854227', 'NCT03922997', 'NCT03976375', 'NCT04023617', 'NCT04036682', 'NCT04044170', 'NCT04069936', 'NCT04154956', 'NCT04207775', 'NCT04303780', 'NCT04331626', 'NCT04364620', 'NCT04394624', 'NCT04413227', 'NCT04471415', 'NCT04471428', 'NCT04612673', 'NCT04614103', 'NCT04619004', 'NCT04640935', 'NCT04644237', 'NCT04656652', 'NCT04667234', 'NCT04685135', 'NCT04790682', 'NCT04811001', 'NCT04816214', 'NCT04868877', 'NCT04878107', 'NCT04884282', 'NCT05195632', 'NCT05255302', 'NCT05361174', 'NCT05364073', 'NCT05378763', 'NCT05403385', 'NCT05407155', 'NCT05488314', 'NCT05555732', 'NCT05577715', 'NCT05599789', 'NCT05631249', 'NCT05661240', 'NCT05796726', 'NCT05834348', 'NCT05862194', 'NCT05935774', 'NCT06048705', 'NCT06068153', 'NCT06106802', 'NCT06279728', 'NCT06388031', 'NCT06394674', 'NCT06487156', 'NCT06497556', 'NCT06523673', 'NCT06542731', 'NCT06555263', 'NCT06557967', 'NCT06558799', 'NCT06667076', 'NCT06668103', 'NCT06686771', 'NCT06690671', 'NCT06759857', 'NCT06761976', 'NCT06809764', 'NCT06813664', 'NCT06929936', 'NCT06933329', 'NCT07111104', 'NCT07130032', 'NCT07140016', 'NCT07144280', 'NCT07154368', 'NCT07164170', 'NCT07174388', 'NCT07185997', 'NCT07193160', 'NCT07213076', 'NCT07242274', 'NCT07246863', 'NCT07288034', 'NCT07363811', 'NCT07420439', 'NCT07463677', 'NCT07609251']) 
+              -- AND rg.result_type = 'Outcome'
+              -- AND rg.group_type IN (
+              --       'Active Comparator',
+              --       'Placebo Comparator',
+              --       'Sham Comparator',
+              --       'No Intervention'
+              -- )
+"""
 # given ids, pull data from db.studies table or aact db.
 
 # n reported events
@@ -240,3 +200,5 @@ WHERE t.nct_id IN ('NCT07416058', 'NCT05769764', 'NCT05002270', 'NCT05786924', '
 SELECT t.* FROM study_references as t 
 WHERE t.nct_id IN ('NCT07416058', 'NCT05769764', 'NCT05002270', 'NCT05786924', 'NCT05578092', 'NCT06119581', 'NCT05480865', 'NCT06026410', 'NCT06793215', 'NCT05485974', 'NCT05853575', 'NCT06447662', 'NCT04975256', 'NCT04699188', 'NCT05375994', 'NCT07601048', 'NCT06497556', 'NCT06300177', 'NCT07288034', 'NCT04330664', 'NCT05492045', 'NCT05840510', 'NCT06481813', 'NCT04585035', 'NCT07543172', 'NCT05074810', 'NCT04967079', 'NCT05585320', 'NCT06130254', 'NCT05374538', 'NCT05358249', 'NCT05288205')
 """
+"['NCT07416058', 'NCT05769764', 'NCT05002270', 'NCT05786924', 'NCT05578092', 'NCT06119581', 'NCT05480865', 'NCT06026410', 'NCT06793215', 'NCT05485974', 'NCT05853575', 'NCT06447662', 'NCT04975256', 'NCT04699188', 'NCT05375994', 'NCT07601048', 'NCT06497556', 'NCT06300177', 'NCT07288034', 'NCT04330664', 'NCT05492045', 'NCT05840510', 'NCT06481813', 'NCT04585035', 'NCT07543172', 'NCT05074810', 'NCT04967079', 'NCT05585320', 'NCT06130254', 'NCT05374538', 'NCT05358249', 'NCT05288205']"
+"['NCT00005037', 'NCT00005093', 'NCT00021008', 'NCT00022022', 'NCT00022243', 'NCT00029003', 'NCT00030420', 'NCT00030641', 'NCT00033722', 'NCT00039091', 'NCT00040885', 'NCT00045006', 'NCT00047840', 'NCT00054184', 'NCT00062101', 'NCT00064012', 'NCT00066768', 'NCT00073008', 'NCT00074204', 'NCT00075751', 'NCT00077415', 'NCT00114192', 'NCT00139711', 'NCT00160043', 'NCT00160069', 'NCT00161187', 'NCT00228358', 'NCT00238849', 'NCT00268489', 'NCT00278187', 'NCT00300586', 'NCT00305786', 'NCT00327288', 'NCT00343720', 'NCT00346645', 'NCT00359450', 'NCT00362882', 'NCT00388206', 'NCT00398138', 'NCT00400829', 'NCT00445952', 'NCT00447057', 'NCT00454194', 'NCT00499733', 'NCT00519831', 'NCT00520845', 'NCT00528281', 'NCT00530621', 'NCT00532155', 'NCT00534209', 'NCT00538850', 'NCT00638937', 'NCT00693992', 'NCT00697060', 'NCT00698815', 'NCT00738335', 'NCT00738881', 'NCT00754923', 'NCT00777309', 'NCT00989690', 'NCT01022671', 'NCT01057212', 'NCT01068587', 'NCT01107444', 'NCT01124669', 'NCT01168973', 'NCT01183858', 'NCT01204697', 'NCT01210053', 'NCT01248247', 'NCT01344824', 'NCT01362296', 'NCT01395758', 'NCT01438307', 'NCT01526928', 'NCT01620190', 'NCT01630733', 'NCT01658813', 'NCT01664533', 'NCT01708993', 'NCT01717105', 'NCT01725165', 'NCT01750281', 'NCT01773109', 'NCT01783197', 'NCT01783834', 'NCT01784640', 'NCT01846416', 'NCT01866410', 'NCT01915524', 'NCT01933932', 'NCT01982955', 'NCT01999673', 'NCT02041468', 'NCT02069418', 'NCT02132884', 'NCT02134990', 'NCT02152631', 'NCT02208843', 'NCT02250326', 'NCT02298153', 'NCT02330367', 'NCT02346370', 'NCT02424617', 'NCT02451865', 'NCT02463994', 'NCT02503358', 'NCT02520778', 'NCT02546986', 'NCT02592577', 'NCT02613507', 'NCT02623595', 'NCT02658097', 'NCT02673814', 'NCT02750215', 'NCT02823990', 'NCT02879760', 'NCT02950038', 'NCT02959437', 'NCT02967133', 'NCT02976740', 'NCT02981108', 'NCT03023423', 'NCT03048500', 'NCT03053297', 'NCT03085069', 'NCT03087448', 'NCT03133546', 'NCT03138889', 'NCT03158883', 'NCT03168464', 'NCT03176173', 'NCT03273790', 'NCT03285763', 'NCT03288870', 'NCT03304093', 'NCT03370159', 'NCT03382912', 'NCT03452592', 'NCT03468985', 'NCT03469960', 'NCT03502850', 'NCT03512847', 'NCT03669523', 'NCT03735121', 'NCT03801304', 'NCT03854227', 'NCT03922997', 'NCT03976375', 'NCT04023617', 'NCT04036682', 'NCT04044170', 'NCT04069936', 'NCT04154956', 'NCT04207775', 'NCT04303780', 'NCT04331626', 'NCT04364620', 'NCT04394624', 'NCT04413227', 'NCT04471415', 'NCT04471428', 'NCT04612673', 'NCT04614103', 'NCT04619004', 'NCT04640935', 'NCT04644237', 'NCT04656652', 'NCT04667234', 'NCT04685135', 'NCT04790682', 'NCT04811001', 'NCT04816214', 'NCT04868877', 'NCT04878107', 'NCT04884282', 'NCT05195632', 'NCT05255302', 'NCT05361174', 'NCT05364073', 'NCT05378763', 'NCT05403385', 'NCT05407155', 'NCT05488314', 'NCT05555732', 'NCT05577715', 'NCT05599789', 'NCT05631249', 'NCT05661240', 'NCT05796726', 'NCT05834348', 'NCT05862194', 'NCT05935774', 'NCT06048705', 'NCT06068153', 'NCT06106802', 'NCT06279728', 'NCT06388031', 'NCT06394674', 'NCT06487156', 'NCT06497556', 'NCT06523673', 'NCT06542731', 'NCT06555263', 'NCT06557967', 'NCT06558799', 'NCT06667076', 'NCT06668103', 'NCT06686771', 'NCT06690671', 'NCT06759857', 'NCT06761976', 'NCT06809764', 'NCT06813664', 'NCT06929936', 'NCT06933329', 'NCT07111104', 'NCT07130032', 'NCT07140016', 'NCT07144280', 'NCT07154368', 'NCT07164170', 'NCT07174388', 'NCT07185997', 'NCT07193160', 'NCT07213076', 'NCT07242274', 'NCT07246863', 'NCT07288034', 'NCT07363811', 'NCT07420439', 'NCT07463677', 'NCT07609251']"
--- data/aact_data_guide.md
+++ data/aact_data_guide.md
@@ -11,3 +11,5 @@ For example, Outcomes.result_group_id = Result_Groups.id.
 
 ## the problem
 design groups spcify the experimental arm, vs control vs other arms, but the results section reports things in groups, but it does not necessarily use the same groups/arms and it doesn't specify which ones are the experimental/control/etc arms. for now, I think I should just pick one... Overall, results are not commonly too commonly reported to ctgov it appears. I think something like 13%
+
+I want to try joining by group title, across design group, and result_groups
